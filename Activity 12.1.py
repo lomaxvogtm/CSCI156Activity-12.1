e9fc647be1e205acb33e29212af06a46a6a75c03
lomaxvogtm <mcl9@alfred.edu>
2014-11-03 14:43:00 -0500

    12.1

---
+++ Activity 12.1.py
@@ -0,0 +1,23 @@
+__author__ = 'Madeleine'
+
+
+def splitemail(email):
+    a = [".com", ".gov", ".edu", ".org", ".mil", ".net"]
+    try:
+        username, domain = email.split("@")
+        domain, gTLD = domain.split(".")
+        print(username,domain, gTLD)
+        if gTLD in a:
+            print(domain)
+            print(gTLD)
+            print(username)
+    except ValueError:
+        print("Please enter a valid email address.")
+
+
+
+# inputemail('.edu', '.org',)
+
+emailaddress = input("Type in an email address, don't screw it up Bowers whannabe.")
+splitemail(emailaddress)
+
